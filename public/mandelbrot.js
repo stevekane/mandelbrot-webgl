161faeb9d362a3529ed37abe96b272898f672eee
Steve Kane <kanesteven@gmail.com>
2014-12-30 16:53:25 -0600

    moved matrices to CPU and built support for full screen

--- public/mandelbrot.js
+++ public/mandelbrot.js
@@ -14,28 +14,57 @@ var quadVerts = new Float32Array([
   1, 1, -1, -1, 1, -1
 ])
 
-function Location (x, y, scale, name) {
+function Location (x, y, scale, theta, name) {
   this.x     = x
   this.y     = y
   this.scale = scale
+  this.theta = theta
   this.name  = name
 }
 
-function copy (obj) {
-  var c = {}
-
-  Object.keys(obj).forEach(function (key) {
-    c[key] = obj[key] 
-  })
-  return c
-}
-
 function Scope (initialLocation) {
+  var zoomScale = Mat3()
+  var rotation  = Mat3()
+  var center    = Mat3()
+
   this.location = copy(initialLocation)
-  this.target = initialLocation
+  this.target   = initialLocation
+
+  Object.defineProperty(this, "zoomScale", {
+    get: function () {
+      zoomScale[0] = this.location.scale 
+      zoomScale[4] = this.location.scale 
+      return zoomScale
+    }, 
+  })
+
+  Object.defineProperty(this, "rotation", {
+    get: function () {
+      var theta = this.location.theta
+      var cos   = Math.cos(theta)
+      var sin   = Math.sin(theta)
+
+      rotation[0] = cos
+      rotation[1] = -sin
+      rotation[3] = sin
+      rotation[4] = cos
+      return rotation
+    } 
+  })
+
+  Object.defineProperty(this, "center", {
+    get: function () {
+      center[6] = this.location.x     
+      center[7] = this.location.y
+      return center
+    } 
+  })
 
   Object.defineProperty(this, "scaleDiff", {
     get: function () { return this.target.scale - this.location.scale }
+  })
+  Object.defineProperty(this, "thetaDiff", {
+    get: function () { return this.target.theta - this.location.theta }
   })
   Object.defineProperty(this, "xDiff", {
     get: function () { return this.target.x - this.location.x }
@@ -50,18 +79,21 @@ var locations = {
     0,
     0,
     10,
+    0,
     "Base"
   ),
   minibrot: new Location(
     -1.2561516034985423,
     0.3808247882043751,
     5.634342535442616e-2,
+    Math.PI / 3,
     "MiniBrot"
   ),
   spirals: new Location(
     0.28693186889504513,
     0.014286693904085048,
     10e-5,
+    Math.PI,
     "Spirals"
   )
 }
@@ -83,7 +115,6 @@ function makeUpdate () {
   var dT      = newTime - oldTime
   var scaleFactor = .05
 
-
   return function update () {
     oldTime = newTime
     newTime = Date.now()
@@ -93,6 +124,7 @@ function makeUpdate () {
     scope.target = timeline.currentFrame.data
     
     scope.location.scale += scope.scaleDiff * scaleFactor
+    scope.location.theta += scope.thetaDiff * scaleFactor
     scope.location.x     += scope.xDiff * scaleFactor
     scope.location.y     += scope.yDiff * scaleFactor
   }
@@ -100,9 +132,10 @@ function makeUpdate () {
 
 
 function makeRender () {
-  var centerLoc     = program.uniforms.center
-  var scaleLoc      = program.uniforms.scale
   var screenSizeLoc = program.uniforms.screenSize
+  var centerLoc     = program.uniforms.center
+  var zoomScaleLoc  = program.uniforms.zoomScale
+  var rotationLoc   = program.uniforms.rotation
 
   gl.enable(gl.BLEND)
   gl.enable(gl.CULL_FACE)
@@ -116,19 +149,24 @@ function makeRender () {
   return function render () {
     title.textContent = scope.target.name
     gl.uniform2f(screenSizeLoc, gl.drawingBufferWidth, gl.drawingBufferHeight)
-    gl.uniform2f(centerLoc, scope.location.x, scope.location.y)
-    gl.uniform1f(scaleLoc, scope.location.scale)
+    gl.uniformMatrix3fv(centerLoc, gl.FALSE, scope.center)
+    gl.uniformMatrix3fv(zoomScaleLoc, gl.FALSE, scope.zoomScale)
+    gl.uniformMatrix3fv(rotationLoc, gl.FALSE, scope.rotation)
     gl.drawArrays(gl.TRIANGLES, 0, 6)
     requestAnimationFrame(render) 
   }
 }
 
 function resize (width, height) {
-  var size = width < height ? width : height
+  var ratio       = 1920 / 1080
+  var targetRatio = width / height
+  var useWidth    = ratio >= targetRatio
+  var newWidth    = useWidth ? width : (height * ratio) 
+  var newHeight   = useWidth ? (width / ratio) : height
 
-  canvas.width  = size
-  canvas.height = size
-  gl.viewport(0, 0, size, size)
+  canvas.width  = newWidth 
+  canvas.height = newHeight 
+  gl.viewport(0, 0, newWidth, newHeight)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
--- public/utils.js
+++ public/utils.js
@@ -6,3 +6,13 @@ function copy (obj) {
   })
   return c
 }
+
+function Mat3 () {
+  var mat = new Float32Array(9)
+
+  mat[0] = 1
+  mat[4] = 1
+  mat[8] = 1
+
+  return mat
+}
